532355de50ecf9c30a57b6f707f4327d6c344a8e
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2024-05-21 17:35:46 -0300

    add dropzone component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { Dropzone } from "@/components/dropzone";
+import { auth } from "@clerk/nextjs/server";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { userId } = auth();
+
+  return (
+    <div>
+      <Dropzone />
+    </div>
+  );
 }
