cb533cbdc70f7d765e1b093edb3a7e18064d2658
Leandro Amorim Salles <leandrosalles1878@gmail.com>
2024-05-21 17:37:35 -0300

    style: update icon and spacings

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,23 +5,25 @@ import Link from "next/link";
 
 import logo from "../assets/dropbox-logo.svg";
 import { UserButton } from "@clerk/nextjs";
-import { SignInButton, SignedOut } from "@clerk/clerk-react";
+import { SignInButton, SignedOut, SignUpButton } from "@clerk/clerk-react";
+import { ModeToggle } from "./theme/theme-toggler";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between">
-      <Link href="/" className="flex items-center gap-2">
+    <header className="flex items-center justify-between px-6 py-4">
+      <Link href="/" className="flex items-center gap-4">
         <div className="bg-[#0866FF] p-2">
-          <Image src={logo} alt="Dropbox Logo" width={40} height={40} />
+          <Image src={logo} alt="Dropbox Logo" width={25} height={25} />
         </div>
-        <h1 className="font-bold text-xl">Dropbox</h1>
+        <h1 className="font-bold text-2xl tracking-tighter">Dropclone</h1>
       </Link>
-      <div>
-        {/* Theme button */}
+      <div className="flex items-center gap-2">
+        <ModeToggle />
+
         <UserButton afterSignOutUrl="/" />
 
         <SignedOut>
-          <SignInButton signUpFallbackRedirectUrl="/dashboard" />
+          <SignInButton signUpFallbackRedirectUrl="/dashboard" mode="modal" />
         </SignedOut>
       </div>
     </header>
